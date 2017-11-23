474c39e2f3d68613c2eeedf0a97056b433dd1694
Jose Nolberto Vilchez Moreno <jose.vilchez.m@upch.pe>
2017-11-23 14:53:39 -0500

    Assets Module

--- module/Generator.php
+++ module/Generator.php
@@ -12,4 +12,19 @@ class Generator extends \yii\gii\generators\module\Generator {
         return 'Generador de Modulos para Insite.pe';
     }
 
+    public function requiredTemplates() {
+        return ['module.php', 'controller.php', 'view.php', 'assets.php'];
+    }
+
+    public function generate() {
+
+        $files      = parent::generate();
+        $modulePath = $this->getModulePath();
+        $files[]    = new CodeFile(
+                $modulePath . "/config/" . ucfirst($this->moduleID) . "Asset.php", $this->render("assets.php")
+        );
+
+        return $files;
+    }
+
 }
